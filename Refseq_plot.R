dd618b0c0e8f0affccca0ed3e956cfbb7b2587d7
BRYZAV <97570002+BRYZAV@users.noreply.github.com>
2022-08-08 13:09:51 -0400

    Update Refseq_plot.R

--- Refseq_plot.R
+++ Refseq_plot.R
@@ -1,8 +1,8 @@
 > library(ggplot2)
 > library(dplyr)
 
-> fungi_data <- read.csv("fungi_bat.csv", header = T, sep = ",")
+> file_name_data <- read.csv("file_name.csv", header = T, sep = ",")
 
-> big_hits <- filter(fungi_data, Hits > 10)
+> big_hits <- filter(file_name_data, Hits > 10)
 
 > ggplot(big_hits, aes(color = Microbe_organism, y=Hits, x= Bat_species)) + geom_point(size=10) + theme_ipsum() + coord_flip()
