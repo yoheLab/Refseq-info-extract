9df26dc65b296261abd9e577bde9ee6efffb3223
BRYZAV <97570002+BRYZAV@users.noreply.github.com>
2022-08-20 14:28:54 -0400

    Update Refseq_plot.R

--- Refseq_plot.R
+++ Refseq_plot.R
@@ -2,8 +2,10 @@
 > library(dplyr)
 
 > file_name_data <- read.csv("file_name.csv", header = T, sep = ",")
+#reading the file to a variable
 
 > big_hits <- filter(file_name_data, Hits > 10)
+#sorts the data by choosing hits greater than 10 to enhcance graph
 
 > ggplot(big_hits, aes(color = Microbe_organism, y=Hits, x= Bat_species)) + geom_point(size=10) + theme_ipsum() + coord_flip()
 #this is a horizontal point graph
